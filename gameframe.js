ac783970c75eaf81c7a4405f470fc224d220d02b
Noah Shutty <noajshu@0587364000.wireless.umich.net>
2015-01-17 14:36:59 -0500

    updated game

--- gameframe.js
+++ gameframe.js
@@ -2,7 +2,7 @@ console.log('hi');
 
 
 
-function playerEnter(){
+function playerEnter(event){
 	console.log("pressed enter");
 }
 
@@ -19,4 +19,30 @@ function playerEnter(){
 var userReg = RegExp("ab*c");
 
 var cmdBox = document.getElementById("userCommand")
-cmdBox.onkeydown = playerEnter;
+
+cmdBox.addEventListener("keydown", function (event) {
+  if (event.defaultPrevented) {
+    return; // Should do nothing if the key event was already consumed.
+  }
+
+  var handled = false;
+  if (event.key !== undefined) {
+    // Handle the event with KeyboardEvent.key and set handled true.
+  } else if (event.keyIdentifier !== undefined) {
+    // Handle the event with KeyboardEvent.keyIdentifier and set handled true.
+  } else if (event.keyCode !== undefined) {
+    console.log(event.keyCode);
+    // Handle the event with KeyboardEvent.keyCode and set handled true.
+  }
+
+  if (handled) {
+    // Consume the event for suppressing "double action".
+    event.preventDefault();
+  }
+}, true);
+
+
+
+
+
+
