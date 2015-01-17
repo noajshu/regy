efa53e2b2e96c52cbbb954c104116f57e01853ea
Noah Shutty <noajshu@0587364000.wireless.umich.net>
2015-01-17 14:07:50 -0500

    updated game

--- gameframe.js
+++ gameframe.js
@@ -1,11 +1,22 @@
 console.log('hi');
 
-var userReg = RegExp("ab*c");
-
-// var cmdBox = document.getElementById("userComman")
-// cmdBox.onkeydown
 
 
 function playerEnter(){
 	console.log("pressed enter");
 }
+
+
+
+
+
+
+
+
+
+
+
+var userReg = RegExp("ab*c");
+
+var cmdBox = document.getElementById("userComman")
+cmdBox.onkeydown = playerEnter;
