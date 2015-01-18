f789ffcb83b5a9cb0cacc199aba3919771b0d8bf
Noah Shutty <noajshu@0587364000.wireless.umich.net>
2015-01-17 20:27:33 -0500

    level support zomg

--- levelHandler.js
+++ levelHandler.js
@@ -16,6 +16,7 @@ function getrV(avgSpeed){
 	vel = avgSpeed * getRandomFactor();
 	console.log('vel = '+vel.toString());
 	angle = getRandomFactor() * 3.14159 / 2;
+	console.log('angle = '+angle.toString());
 	console.log({x:Math.sin(vel), y:Math.cos(vel)});
 	return({x:Math.sin(vel), y:Math.cos(vel)});
 }
