16d0342bbe12e79221c7ba406bc47a298e461f3a
Noah Shutty <noajshu@0587363158.wireless.umich.net>
2015-01-17 21:54:04 -0500

    adding multi-level switching

--- gameframe.js
+++ gameframe.js
@@ -1,3 +1,84 @@
+console.log('hi');
+
+var url = $.url();
+
+var queryLevel = url.attr('query').split('&')[0].split('=')[1];
+// if(queryLevel != undefined){
+// 	theLevel = parseInt(queryLevel);
+// }
+
+console.log("current level = ");
+console.log(theLevel);
+
+var cmdBox = document.getElementById("userCommand");
+var consoleDiv = document.getElementById("userConsole");
+var userView = document.getElementById("userView");
+
+var c = document.getElementById("userCanvas");
+var ctx = c.getContext("2d");
+
+
+function getRandomFactor() { //get random factor
+	return Math.random()/2 + 0.75;
+}
+
+function getrV(avgSpeed){
+	vel = avgSpeed * getRandomFactor();
+	angle = getRandomFactor() * 3.14159 / 2;
+	return({x:vel*Math.sin(angle), y:vel*Math.cos(angle)});
+}
+
+function getrP(){
+	return({x:0, y:document.getElementById("userCanvas").height * getRandomFactor()/2});
+}
+
+var sharkAvgSpeed = 2;
+var catAvgSpeed = 1.7;
+
+function avgShark(theBody) {
+	return {v:getrV(sharkAvgSpeed), pos:getrP(), type:"evil", body:theBody };
+}
+
+function avgCat(theBody) {
+	return {v:getrV(catAvgSpeed), pos:getrP(), type:"good", body:theBody };
+}
+
+
+
+
+var level0 = {levelTargets:[avgShark("evil_shark")], levelInfo:"This is an evil shark"};
+var level1 = {levelTargets:[avgCat("nice_cat")], levelInfo:"Type nothing--do not kill the cat"};
+var level1 = {levelTargets:[avgCat("animal0"), avgShark("animal1")], levelInfo:"Type nothing--do not kill the cat"};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var levelData =[level0, level1];
+
+
+
+
+
+
+
+
+
+
 var catImg = new Image();
 catImg.src = "pixel_cat_1.png";
 var aspectCat = 1.56;
@@ -17,7 +98,6 @@ var yMargin = 15;
 
 // var targetArray = [{type:"evil", body:"shark", pos:{x:40, y:250}, v:{x:0.3, y:0.1}}, {type:"good", body:"meow5_21.tps", pos:{x:30, y:40}, v:{x:0.2, y:0.1}}];
 
-var theLevel = 1;
 var targetArray = levelData[theLevel].levelTargets;
 infoBox.innerHTML = levelData[theLevel].levelInfo;
 
