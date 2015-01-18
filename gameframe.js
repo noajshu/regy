257695f6a87b11c5414c6f7bbaa9f31d9bc52fcb
Noah Shutty <noajshu@0587364000.wireless.umich.net>
2015-01-17 19:57:00 -0500

    getting newtarget to work

--- gameframe.js
+++ gameframe.js
@@ -24,7 +24,7 @@ var widthShark = heightShark * aspectShark;
 var yMargin = 15;
 
 
-// var targetArray = [{type:"evil", body:"shark", pos:{x:30, y:50}, v:{x:0.3, y:0.1}}, {type:"good", body:"meow5_21.tps", pos:{x:30, y:40}, v:{x:0.2, y:0.1}}];
+var targetArray = [{type:"evil", body:"shark", pos:{x:30, y:50}, v:{x:0.3, y:0.1}}, {type:"good", body:"meow5_21.tps", pos:{x:30, y:40}, v:{x:0.2, y:0.1}}];
 
 var theLevel = 0;
 
